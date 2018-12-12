3c2026112a058da7c11585a191fd7b5aa5f980f4
eches <Elaine.Chesoni83@gmail.com>
2018-12-11 22:11:06 -0800

    modified server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 
 let port = process.env.PORT;
 if( port == null || port == "") {
-  port = 3000;
+  port = 5000;
 }
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
